5725c355f7c49829a3c38653927814e9d690d291
Thomas <thomasreuvers@upcmail.nl>
2021-09-27 17:18:22 +0200

    Add shader upload functions and some basic shader testing code

--- src/main/java/voyage/LevelEditorScene.java
+++ src/main/java/voyage/LevelEditorScene.java
@@ -3,6 +3,7 @@ package voyage;
 import org.joml.Vector2f;
 import org.lwjgl.BufferUtils;
 import renderer.Shader;
+import util.Time;
 
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
@@ -112,6 +113,7 @@ public class LevelEditorScene extends Scene {
         defaultShader.use();
         defaultShader.uploadMat4f("uProjection", camera.getProjectionMatrix());
         defaultShader.uploadMat4f("uView", camera.getViewMatrix());
+        defaultShader.uploadFloat("uTime", Time.getTime());
 
         // Bind the VAO that we're using
         glBindVertexArray(vaoID);
